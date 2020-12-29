7bd3b1ab4765c16067b95ab36c9f3ec649962678
kenanbit <kenan.bitikofer@joinroot.com>
2020-12-28 21:00:04 -0500

    Use dict to wrap weights and biases into network
    This helps the passing of args to be more convenient and succinct

--- main.py
+++ main.py
@@ -6,15 +6,33 @@ from copy import deepcopy
 NODES_PER_LAYER = [784, 16, 16, 10]
 LAYER_COUNT = len(NODES_PER_LAYER)
 
-def new_layer_weights(layer_number):
-    if layer_number < 0 or layer_number >= LAYER_COUNT:
+
+def new_network(nodes_per_layer):
+    layer_count = len(nodes_per_layer)
+    layer_weights = [
+        new_layer_weights(layer, nodes_per_layer)
+        for layer in range(layer_count)
+    ]
+    layer_biases = [
+        new_layer_biases(layer, nodes_per_layer)
+        for layer in range(layer_count)
+    ]
+    return {
+        "weights": layer_weights,
+        "biases": layer_biases
+    }
+
+
+def new_layer_weights(layer_number, nodes_per_layer):
+    layer_count = len(nodes_per_layer)
+    if layer_number < 0 or layer_number >= layer_count:
         raise Exception(f"""layer_number is {layer_number}. It must be
-            between 0 and {LAYER_COUNT-1}""")
+            between 0 and {layer_count-1}""")
     if layer_number == 0:
         return None
 
-    current_node_count = NODES_PER_LAYER[layer_number]
-    previous_node_count = NODES_PER_LAYER[layer_number - 1]
+    current_node_count = nodes_per_layer[layer_number]
+    previous_node_count = nodes_per_layer[layer_number - 1]
     # return np.full(
     #     [current_node_count, previous_node_count],
     #     .01
@@ -27,13 +45,14 @@ def new_layer_weights(layer_number):
     ).T
 
 
-def new_layer_biases(layer_number):
-    if layer_number < 0 or layer_number >= LAYER_COUNT:
+def new_layer_biases(layer_number, nodes_per_layer):
+    layer_count = len(nodes_per_layer)
+    if layer_number < 0 or layer_number >= layer_count:
         raise Exception(f"""layer_number is {layer_number}. It must be
-            between 0 and {LAYER_COUNT-1}""")
+            between 0 and {layer_count-1}""")
     if layer_number == 0:
         return None
-    return np.zeros([NODES_PER_LAYER[layer_number]], dtype='float32')
+    return np.zeros([nodes_per_layer[layer_number]], dtype='float32')
 
 
 def sigmoid(z):
@@ -49,20 +68,20 @@ def i_sigmoid(a):
     return -np.log(1/a - 1)
 
 
-def feed_forward(inputs, weights, biases):
+def feed_forward(inputs, net):
     if type(inputs) != np.ndarray:
         raise Exception(f"inputs is {inputs}. It must be an np.ndarray")
     if type(inputs[0]) != np.ndarray:
         raise Exception(f"inputs[0] is {inputs}. It must be an np.ndarray")
     if type(inputs) != np.ndarray:
         raise Exception(f"inputs is {inputs}. It must be an np.ndarray")
-    if weights[0] is not None:
-        raise Exception("""weights[0] should be None, since the input layer
+    if net['weights'][0] is not None:
+        raise Exception("""net['weights'][0] should be None, since the input layer
             has no incoming weights.""")
-    if type(weights[1]) != np.ndarray:
-        raise Exception("weights elements must be an np.ndarray")
+    if type(net['weights'][1]) != np.ndarray:
+        raise Exception("net['weights'] elements must be an np.ndarray")
 
-    layer_count = len(weights)
+    layer_count = len(net['weights'])
     layer_activations = [None] * layer_count
     # Set the first layer activations manually
     layer_activations[0] = inputs
@@ -70,13 +89,13 @@ def feed_forward(inputs, weights, biases):
     # Find layer_activations forward through the layers
     for layer in range(1, layer_count):
         # This line (matrix multiplication):
-        z_values = weights[layer] @ layer_activations[layer-1] + my_transpose(biases[layer])
+        z_values = net['weights'][layer] @ layer_activations[layer-1] + my_transpose(net['biases'][layer])
         # ... is equivalent to this old code:
         # z_values = np.array([
-        #     sum(layer_activations[layer-1] * weights[layer][j])
-        #     for j in range(len(weights[layer]))
+        #     sum(layer_activations[layer-1] * net['weights'][layer][j])
+        #     for j in range(len(net['weights'][layer]))
         # ])
-        # z_values += biases[layer]
+        # z_values += net['biases'][layer]
         layer_activations[layer] = sigmoid(z_values)
     return layer_activations
 
@@ -90,7 +109,7 @@ def d_cost_single(correct_output, final_activation):
     return correct_output - final_activation
 
 
-## This one takes matrices for both arguments
+# This one takes matrices for both arguments
 def cost_multiple(correct_outputs, activations):
     if type(correct_outputs) != np.ndarray:
         raise Exception(f"correct_outputs is {correct_outputs}. It must be an np.ndarray")
@@ -114,14 +133,18 @@ def delta_final_layer_single(correct_output, final_activation):
     d_sigmoids = d_sigmoid(z_values)
     return np.multiply(cost_gradient, d_sigmoids)
 
-def delta_all_layers_single(layer_weights, correct_output, layer_activations):
+
+def delta_all_layers_single(net, correct_output, layer_activations):
     layer_count = len(layer_activations)
     delta_all_layers = [None] * layer_count
     delta_all_layers[-1] = delta_final_layer_single(correct_output, layer_activations[-1])
 
     for l in range(layer_count-2, 0, -1):
         z_values = i_sigmoid(layer_activations[l])
-        delta_all_layers[l] = np.multiply(layer_weights[l+1].T @ delta_all_layers[l+1], d_sigmoid(z_values))
+        delta_all_layers[l] = np.multiply(
+            net['weights'][l+1].T @ delta_all_layers[l+1],
+            d_sigmoid(z_values)
+        )
 
     return delta_all_layers
 
@@ -137,11 +160,11 @@ def weight_gradients_single(layer_activations, delta_all_layers):
     ]
 
 
-def backpropogate_single(correct_output, layer_weights, layer_biases, layer_activations):
-    bias_gradient = bias_gradients_single(layer_weights, correct_output, layer_activations)
+def backpropogate_single(net, correct_output, layer_activations):
+    bias_gradient = bias_gradients_single(net['weights'], correct_output, layer_activations)
     delta_gradient = bias_gradient
     weight_gradient = weight_gradients_single(layer_activations, delta_gradient)
-    # TODO for each layer, add to existing layer_weights and layer_biases
+    # TODO for each layer, add to existing net['weights'] and net['biases']
     # Then we can check that the backpropagation moves the network in the correct direction
     # Then we can set up batching to
     # and test on MNIST data
@@ -167,15 +190,14 @@ print("Finished loading defs. Time to execute...")
 sleep(1)
 
 # NEW SYSTEM
-all_weights = [new_layer_weights(layer) for layer in range(LAYER_COUNT)]
+net = new_network(NODES_PER_LAYER)
 
-all_biases = [new_layer_biases(layer) for layer in range(LAYER_COUNT)]
 inputs = my_transpose(np.arange(NODES_PER_LAYER[0]) / 784)
 correct_outputs = my_transpose([1,1,1,1,1,1,1,1,1,0])
 
-layer_activations = feed_forward(inputs, all_weights, all_biases)
-delta_all_layers = delta_all_layers_single(all_weights, correct_outputs, layer_activations)
-bias_gradient = bias_gradients_single(all_weights, correct_outputs, layer_activations)
+layer_activations = feed_forward(inputs, net)
+delta_all_layers = delta_all_layers_single(net, correct_outputs, layer_activations)
+bias_gradient = bias_gradients_single(net, correct_outputs, layer_activations)
 weight_gradient = weight_gradients_single(layer_activations, bias_gradient)
 
 if __name__ == "__main__":
